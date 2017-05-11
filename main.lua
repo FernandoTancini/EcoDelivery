c356737254081b73c3e74c8201b8439b1c8a0153
Fernando Baggi Tancini <fernandotancini@hotmail.com>
2017-05-11 12:57:51 -0300

    comecei na aula e vou terminar em casa
    lalalalala

--- main.lua
+++ main.lua
@@ -6,7 +6,19 @@ local world
 local objects
 
 local lucioVelXMax = 500
-local tamanhoDaPista = 8000
+local larguraDoMundo = 8000
+local windowWidth = 800
+local windowHeight = 600
+local leftMarginForMoviment = 150
+local rightMarginForMoviment = 150
+
+local posicaoXJanelaComRelacaoAPista = 0
+local velXJanelaComrelacaoAPista = 0
+
+local backgroundHeightScaleFactor = 1
+local quantidadeDeBackgroundsARepetir = 1
+
+local velocidadeDoPersonagemAntesDaColisao = 0
 
 local isPersonagemTocandoPista = false
 
@@ -18,11 +30,12 @@ function love.load()
   -- background
   background = love.graphics.newImage("imagens/cenario.png")
   backgroundHeightScaleFactor = love.graphics.getHeight() / background:getHeight()
+  quantidadeDeBackgroundsARepetir = larguraDoMundo / (backgroundHeightScaleFactor * background:getWidth())
   
   -- world
   world = ourPhysics.setupWorld()
-  objects = ourPhysics.getObjects(world)
-  world:setCallbacks(beginContact, endContact)
+  objects = ourPhysics.getObjects(world, larguraDoMundo, windowWidth, windowHeight, leftMarginForMoviment, rightMarginForMoviment)
+  world:setCallbacks(beginContact, endContact, preSolve, postSolve)
   
 end
 
@@ -30,7 +43,7 @@ function love.update(dt)
   
   world:update(dt) -- é preciso chamar essa funcao para o phisics atuar com o "seu update"
   
-  -- pegar velocidades para usa abaixo
+  -- pegar velocidade e posicao para usa abaixo
   local lucioVelX, lucioVelY = objects.lucio.body:getLinearVelocity()
   local lucioX, lucioY = objects.lucio.body:getPosition()
   
@@ -53,13 +66,18 @@ function love.update(dt)
     objects.lucio.body:setLinearVelocity(-lucioVelXMax, lucioVelY)
   end
   
+  -- atualizar posicao
+  posicaoXJanelaComRelacaoAPista = posicaoXJanelaComRelacaoAPista + (velXJanelaComrelacaoAPista * dt)
+  
 end
 
 function love.draw()
   
   -- background
   love.graphics.setColor(255,255,255)
-  love.graphics.draw(background, 0, 0, 0, backgroundHeightScaleFactor, backgroundHeightScaleFactor)
+  for i = 0, love.graphics.getWidth() / background:getWidth() do
+    love.graphics.draw(background, (i * background:getWidth() * backgroundHeightScaleFactor) - posicaoXJanelaComRelacaoAPista, 0, 0, backgroundHeightScaleFactor, backgroundHeightScaleFactor)
+  end
   
   ourPhysics.draw(objects)
   
@@ -67,16 +85,52 @@ function love.draw()
 end
 
 function beginContact(a, b, coll)
+  -- colisao personagem - chao
   if (a:getUserData() == "personagem" and b:getUserData() == "pista") or (a:getUserData() == "pista" and b:getUserData() == "personagem") then
-    -- teve contato entre pista e personagem
     isPersonagemTocandoPista = true
   end
 end
  
 function endContact(a, b, coll)
+  -- colisao personagem - chao
   if (a:getUserData() == "personagem" and b:getUserData() == "pista") or (a:getUserData() == "pista" and b:getUserData() == "personagem") then
-    -- acabaou contato entre pista e personagem
     isPersonagemTocandoPista = false
+  end
+  
+  -- colisao personagem - rightColider
+  if (a:getUserData() == "personagem" and b:getUserData() == "rightColider") or (a:getUserData() == "rightColider" and b:getUserData() == "personagem") then
+    velXJanelaComrelacaoAPista = 0
+  end
+  
+  -- colisao personagem - leftColider
+  if (a:getUserData() == "personagem" and b:getUserData() == "leftColider") or (a:getUserData() == "leftColider" and b:getUserData() == "personagem") then
+    velXJanelaComrelacaoAPista = 0
+  end
+end
+
+function preSolve(a, b, coll)
+ -- colisao personagem - rightColider
+  if (a:getUserData() == "personagem" and b:getUserData() == "rightColider") or (a:getUserData() == "rightColider" and b:getUserData() == "personagem") then
+    velocidadeDoPersonagemAntesDaColisao, lixo = a:getLinearVelocity()
+  end
+  
+  -- colisao personagem - leftColider
+  if (a:getUserData() == "personagem" and b:getUserData() == "leftColider") or (a:getUserData() == "leftColider" and b:getUserData() == "personagem") then
+    
+  end
+end
+ 
+function postSolve(a, b, coll, normalimpulse, tangentimpulse)
+ -- colisao personagem - rightColider
+  if (a:getUserData() == "personagem" and b:getUserData() == "rightColider") or (a:getUserData() == "rightColider" and b:getUserData() == "personagem") then
+    velXJanelaComrelacaoAPista = normalimpulse
+    xx, yy = a:getVelocity()
+    a:setVelocity(velocidadeDoPersonagemAntesDaColisao, yy)
+  end
+  
+  -- colisao personagem - leftColider
+  if (a:getUserData() == "personagem" and b:getUserData() == "leftColider") or (a:getUserData() == "leftColider" and b:getUserData() == "personagem") then
+    velXJanelaComrelacaoAPista = - normalimpulse
   end
 end
 
--- ourPhysics.lua
+++ ourPhysics.lua
@@ -5,23 +5,37 @@ function ourPhysics.setupWorld()
   return love.physics.newWorld(0, 9.81*64, true)
 end
 
-function ourPhysics.getObjects(world)
+function ourPhysics.getObjects(world, worldWidth, windowWidth, windowHeight, leftMarginForMoviment, rightMarginForMoviment)
   
   local objects = {} -- tabela para armazenar os objetos
  
   -- ground
   objects.ground = {}
-  objects.ground.body = love.physics.newBody(world, 800/2, 600-150/2) -- esse pontos são o centro do corpo, que tem 800 de largura e 150 de altura
-  objects.ground.shape = love.physics.newRectangleShape(800, 150) -- faz o retangulo com largura 800 e altura 150
+  objects.ground.body = love.physics.newBody(world, worldWidth/2, 600-150/2) -- esse pontos são o centro do corpo, que tem 800 de largura e 150 de altura
+  objects.ground.shape = love.physics.newRectangleShape(worldWidth, 150) -- faz o retangulo com largura 800 e altura 150
   objects.ground.fixture = love.physics.newFixture(objects.ground.body, objects.ground.shape); --attach shape to body
   objects.ground.fixture:setUserData("pista")
   
   -- personagem
   objects.lucio = {}
-  objects.lucio.body = love.physics.newBody(world, 800/2, 600/2, "dynamic") --  ponto incial desse body é o centro da tela e "dynamic" que possibilita o movimento
+  objects.lucio.body = love.physics.newBody(world, leftMarginForMoviment + 20, windowHeight/2, "dynamic") --  ponto incial desse body é o centro da tela e "dynamic" que possibilita o movimento
   objects.lucio.shape = love.physics.newCircleShape(20) -- 20 é o raio do circulo
   objects.lucio.fixture = love.physics.newFixture(objects.lucio.body, objects.lucio.shape, 1) -- Attach fixture to body and give it a density of 1
   objects.lucio.fixture:setUserData("personagem")
+  
+  -- MarginForMoviment
+  -- left
+  objects.leftColider = {}
+  objects.leftColider.body = love.physics.newBody(world, leftMarginForMoviment/2, windowHeight/2, "static")
+  objects.leftColider.shape = love.physics.newRectangleShape(leftMarginForMoviment, windowHeight)
+  objects.leftColider.fixture = love.physics.newFixture(objects.leftColider.body, objects.leftColider.shape)
+  objects.leftColider.fixture:setUserData("leftColider")
+  -- right
+  objects.rightColider = {}
+  objects.rightColider.body = love.physics.newBody(world, windowWidth - (rightMarginForMoviment/2) , windowHeight/2, "static")
+  objects.rightColider.shape = love.physics.newRectangleShape(leftMarginForMoviment, windowHeight)
+  objects.rightColider.fixture = love.physics.newFixture(objects.rightColider.body, objects.rightColider.shape)
+  objects.rightColider.fixture:setUserData("rightColider")
   
   
   return objects
