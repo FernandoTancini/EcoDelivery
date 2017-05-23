00776b8cc7515ffbe108be4ccb4584cb87e7f81a
leohermogenio <Leo@Leonardos-MacBook-Pro.local>
2017-05-23 11:35:09 -0300

    commit p leoAula
    lalalala

--- main.lua
+++ main.lua
@@ -1,31 +1,44 @@
 
+
 local background
 local lucio
 local ourPhysics
 local world
 local objects
 
-local lucioVelXMax = 5000
-local tamanhoDaPista = 8000
-local alturaDaPista = 740
+local lucioVelXMax = 500
+local tamanhoDaPista = 2000
 
 local isPersonagemTocandoPistaL = false
 local isPersonagemTocandoPistaR = false
 local isPersonagemTocandoPistaT = false
 local reset = false
 
+hero = {
+hero_walk = {},
+hero_anim_frame = 1,
+hero_pos_x = 400,
+hero_pos_y = 300,
+hero_anim_time = 0 ,
+}
+
+
 function love.load()
+  
+  for x = 1, 4, 1 do
+    hero.hero_walk[x] = love.graphics.newImage ("imagens/lucio0" .. x .. ".png")
+  end
   
   -- requires
   ourPhysics = require "ourPhysics"
   
   -- background
   background = love.graphics.newImage("imagens/cenario.png")
-  backgroundHeightScaleFactor = alturaDaPista / background:getHeight()
+  backgroundHeightScaleFactor = love.graphics.getHeight() / background:getHeight()
   
   -- world
   world = ourPhysics.setupWorld()
-  objects = ourPhysics.getObjects(world, tamanhoDaPista, alturaDaPista)
+  objects = ourPhysics.getObjects(world, tamanhoDaPista)
   world:setCallbacks(beginContact, endContact)
 
 end
@@ -37,6 +50,7 @@ function love.update(dt)
     objects.lucioL.body:setLinearVelocity(0, 0)
     objects.lucioR.body:setLinearVelocity(0, 0)
     objects.lucioT.body:setLinearVelocity(0, 0)
+    objects.lucioT2.body:setLinearVelocity(0, 0)
     
     else reset = false
   end
@@ -53,15 +67,19 @@ function love.update(dt)
     objects.lucioL.body:setAngularVelocity(0)
     objects.lucioR.body:setAngularVelocity(0)
     objects.lucioT.body:setAngularVelocity(0)
+    objects.lucioT2.body:setAngularVelocity(0)
     objects.lucioL.body:setAngle(0)
     objects.lucioR.body:setAngle(0)
     objects.lucioT.body:setAngle(0)
+    objects.lucioT2.body:setAngle(0)
     objects.lucioL.body:setLinearVelocity(0, 0)
     objects.lucioR.body:setLinearVelocity(0, 0)
     objects.lucioT.body:setLinearVelocity(0, 0)
+    objects.lucioT2.body:setLinearVelocity(0, 0)
     objects.lucioL.body:setPosition(800/2, 600/2)
     objects.lucioR.body:setPosition(800/2+40, 600/2)
-    objects.lucioT.body:setPosition(800/2+20, 600/2-20)
+    objects.lucioT.body:setPosition(objects.lucioL.body:getX()-10, objects.lucioL.body:getY()-60)
+    objects.lucioT2.body:setPosition(objects.lucioL.body:getX()+20, objects.lucioL.body:getY()-25)
     
   end
   
@@ -70,6 +88,18 @@ function love.update(dt)
     objects.lucioL.body:applyForce(200, 0)
     objects.lucioR.body:applyForce(200, 0)
     objects.lucioT.body:setAngularVelocity(0)
+    
+    
+    hero.hero_anim_time = hero.hero_anim_time + dt --incrementa o tempo usando dt
+      if hero.hero_anim_time > 0.1 then --quando acumular mais de 0.1
+        hero.hero_anim_frame = hero.hero_anim_frame + 1 --avana para proximo frame
+          if hero.hero_anim_frame > 4 then
+            hero.hero_anim_frame = 1
+          end
+        hero.hero_anim_time = 0                
+                --reinicializa a contagem do tempo
+      end
+    
 
   end
   if love.keyboard.isDown("down") then
@@ -80,18 +110,18 @@ function love.update(dt)
   end
 
   if love.keyboard.isDown("right") then
-    objects.lucioT.body:setAngularVelocity(math.pi*2)
+    objects.lucioT2.body:setAngularVelocity(math.pi*1)
   end
   
   if love.keyboard.isDown("left") then
-    objects.lucioT.body:setAngularVelocity(-math.pi*3)
+    objects.lucioT2.body:setAngularVelocity(-math.pi*2)
   end
   
   
   if love.keyboard.isDown("space") and (isPersonagemTocandoPistaL or isPersonagemTocandoPistaR) then
-    objects.lucioL.body:applyForce(0, -4000)
-    objects.lucioR.body:applyForce(0, -4000)
-    objects.lucioT.body:applyForce(0, 0)
+    objects.lucioL.body:applyForce(0, -3500)
+    objects.lucioR.body:applyForce(0, -3500)
+    objects.lucioT.body:applyForce(0, 000)
 
   end
   
@@ -102,26 +132,38 @@ function love.update(dt)
     objects.lucioL.body:setLinearVelocity(-lucioVelXMax, lucioVelY)
   end
   
+  hero.hero_pos_x = objects.lucioT.body:getX()
+  hero.hero_pos_y = objects.lucioT.body:getY() -- hero.hero_walk[1]:getHeight()
+  
+  
 end
 
 function love.draw()
-  if objects.lucioL.body:getX() > 200 and objects.lucioL.body:getX() < (tamanhoDaPista - 600) then
-    love.graphics.translate(-lucioX+200, -lucioY+450)
-  elseif objects.lucioL.body:getX() < 200 then
-    love.graphics.translate(0, -lucioY+450)
-  elseif objects.lucioL.body:getX() > (tamanhoDaPista - 600) then
-    love.graphics.translate(-tamanhoDaPista + 600 + 200, -lucioY+450)
+  
+  
+  
+  --love.graphics.translate(0, -lucioY+450)
+  if objects.lucioL.body:getX()>200 then
+  love.graphics.translate(-lucioX+200, -lucioY+450)
+else
+  love.graphics.translate(0, -lucioY+450)
   end
   
   -- background
   love.graphics.setColor(255,255,255)
-  for x=0, tamanhoDaPista, (background:getWidth() * backgroundHeightScaleFactor) do
-  love.graphics.draw(background, x, (love.graphics.getHeight() - alturaDaPista), 0, backgroundHeightScaleFactor, backgroundHeightScaleFactor)
+  for x=0, tamanhoDaPista, background:getWidth() do
+  love.graphics.draw(background, x, 0, 0, backgroundHeightScaleFactor, backgroundHeightScaleFactor)
   end
   ourPhysics.draw(objects)
   
   
+  
+  
 end
+
+--[[function math.round(objects.lucio.body:getX(),4)
+  return objects.lucio.body:getX()
+end]]
 
 function beginContact(a, b,  coll)
   if (a:getUserData() == "personagemL" and b:getUserData() == "pista") or (a:getUserData() == "pista" and b:getUserData() == "personagemL") then
@@ -132,7 +174,7 @@ function beginContact(a, b,  coll)
     -- teve contato entre pista e personagem
     isPersonagemTocandoPistaR = true
   end
-  if (a:getUserData() == "personagemT" and b:getUserData() == "pista") or (a:getUserData() == "pista" and b:getUserData() == "personagemT") then
+  if (a:getUserData() == "personagemT2" and b:getUserData() == "pista") or (a:getUserData() == "pista" and b:getUserData() == "personagemT2") then
     -- teve contato entre pista e personagem
     reset = true
   end
